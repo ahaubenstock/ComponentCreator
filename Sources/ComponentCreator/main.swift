d9ed9e251d4be567377a4e059f124f663cbe6259
Adam Haubenstock <aehaubenstock.dev@gmail.com>
2020-02-08 23:58:23 -0500

    Add parsing of both storyboard and swift files and allow differences in swift file when changing it

--- Sources/ComponentCreator/main.swift
+++ Sources/ComponentCreator/main.swift
@@ -1,16 +1,17 @@
 import Foundation
 import RxSwift
 
-guard CommandLine.arguments.count > 1 else {
-    fatalError("No file was specified.")
+guard CommandLine.arguments.count > 2 else {
+    fatalError("Less than two files were specified.")
 }
-let file = CommandLine.arguments[1]
-let url = URL(fileURLWithPath: file)
-guard let data = try? Data(contentsOf: url) else {
+let storyboardPath = CommandLine.arguments[1]
+let swiftPath = CommandLine.arguments[2]
+let storyboardURL = URL(fileURLWithPath: storyboardPath)
+guard let storyboardData = try? Data(contentsOf: storyboardURL) else {
     fatalError("Unable to read the file.")
 }
-let parser = XMLParser(data: data)
-let subclass = parser.rx.didStartElement
+let storyboardParser = XMLParser(data: storyboardData)
+let subclass = storyboardParser.rx.didStartElement
     .filter { $0.element == "viewController" }
     .map { $0.attributes }
     .map { $0["customClass"]! }
@@ -23,11 +24,11 @@ let typeMap = [
     "constraint": "NSLayoutConstraint"
 ]
 let tagNames = Set(typeMap.keys)
-let types = parser.rx.didStartElement
+let types = storyboardParser.rx.didStartElement
     .filter { tagNames.contains($0.element) }
     .map { $0.element }
     .map { typeMap[$0]! }
-let variables = parser.rx.didStartElement
+let variables = storyboardParser.rx.didStartElement
     .filter { $0.element == "userDefinedRuntimeAttribute" }
     .map { $0.attributes }
     .filter { $0.keys.contains("keyPath") }
@@ -35,27 +36,34 @@ let variables = parser.rx.didStartElement
     .map { $0["value"]! }
     .withLatestFrom(types) { (name: $0, type: $1) }
     .map { "@IBOutlet weak var \($0.name): \($0.type)!" }
-    .takeUntil(parser.rx.didEndDocument)
+    .takeUntil(storyboardParser.rx.didEndDocument)
     .toArray()
     .asObservable()
-let dateFormatter = DateFormatter()
-dateFormatter.dateStyle = .short
-dateFormatter.timeStyle = .short
+	.map { $0.joined(separator: "\n\t") }
+let originalSwift = (try? String(contentsOf: URL(fileURLWithPath: swiftPath), encoding: .utf8)) ?? ""
 let swift = Observable.zip(subclass, variables)
-    .map {
-        """
-        //
-        // Last Generated: \(dateFormatter.string(from: Date()))
-        //
-        
-        class \($0): Component {
-        \t\($1.joined(separator: "\n\t"))
-        }
-        
-        """
-    }
+	.map { (subclass: String, variables: String) -> String in
+		if let match = (try! NSRegularExpression(pattern: #"class +\#(subclass) *: *Component.*?\{"#, options: .dotMatchesLineSeparators))
+			.firstMatch(in: originalSwift, range: NSRange(location: 0, length: originalSwift.count)) {
+			let before = originalSwift.prefix(match.range.location + match.range.length)
+			let after = originalSwift.suffix(from: originalSwift.index(originalSwift.startIndex, offsetBy: match.range.location + match.range.length))
+			let remaining = after
+				.split(separator: "\n")
+				.filter { !$0.trimmingCharacters(in: .whitespaces).hasPrefix("@IBOutlet") }
+				.joined(separator: "\n")
+			return "\(before)\n\t\(variables)\n\(remaining)"
+		} else {
+			return
+				"""
+				class \(subclass): Component {
+					\(variables)
+				}
+				
+				"""
+		}
+	}
 _ = swift
-    .subscribe(onNext: { print($0) })
-if !parser.parse() {
+	.subscribe(onNext: { print($0) })
+if !storyboardParser.parse() {
     fatalError("Parsing failed.")
 }
